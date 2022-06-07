866951d730d427ae040b6ab612547c6a240ef53b
Jule Drewalowski <jule.drewa@googlemail.com>
2022-06-07 17:32:04 +0200

    more tests, more documentation

--- R/model-generation.R
+++ R/model-generation.R
@@ -1,8 +1,13 @@
-#' Create a list of slendr populations according to a given tree. Starting from
+#' Create populations based on given tree
+#'
+#' Creates a list of slendr populations according to a given tree. Starting from
 #' the root, the tree is traversed recursively and a new population is created
 #' for every left child of a given node.
 #'
-#' todo explain in more detail (z.b. 3/4)
+#' The simulation length parameter is used to scale the length of the edges of
+#' the tree. The last population is created at 3/4 of the simulation length, so
+#' that it will exist for a bit before the simulation ends. The root of the tree
+#' is the ancestral population, which is created at time 1.
 #'
 #' @param tree An ape tree
 #' @param population_size Integer number defining the populations size of all
@@ -13,31 +18,41 @@
 #' @return List of slendr populations
 #' @examples
 #' tree <- ape::rtree(4)
-#' tree_populations(tree, 1000, 50)
+#' tree_populations(tree = tree, population_size = 1000, simulation_length = 50)
 #' tree <- ape::rtree(6)
-#' tree_populations(tree, 200, 500)
+#' tree_populations(tree = tree, population_size = 200, simulation_length = 500)
 tree_populations <- function(tree, population_size, simulation_length) {
-  # plot to check
-  # plot(tree, show.tip.label = F)
-  # nodelabels()
-  # tiplabels()
+
+  if (length(tree$tip.label) + tree$Nnode < 3) {
+    stop("At least 2 populations must be created", call. = FALSE)
+  }
 
   env <- new.env()
   env$populations <- vector(mode="list", length=tree$Nnode +
                               length(tree$tip.label))
-  ## TODO explain
+
+  # scale defined by maximum edge length of the tree and given simulation length
+  # only 3/4 of this scale used, so that all populations exist at least for 1/4
+  # of the simulation
   scale <- floor((3*simulation_length/4)/max(ape::node.depth.edgelength(tree)))
 
+  # create ancestral population
   root <- length(tree$tip.label) + 1
   env$populations[[root]] <- population(paste0("pop", root), time = 1,
                                         N = population_size)
 
+  # recurse through tree
   recursion(tree, root, env$populations[[root]], population_size, env, scale)
+
+  # remove empty populations from list
   env$populations <- env$populations[-which(sapply(env$populations, is.null))]
+
   return(env$populations)
 }
 
-#' Create a random list of slendr populations of given length. A random tree is
+#' Create random populations
+#'
+#' Creates a random list of slendr populations of given length. A random tree is
 #' created and the function \code{tree_populations} is called to create the
 #' populations.
 #'
@@ -49,16 +64,25 @@ tree_populations <- function(tree, population_size, simulation_length) {
 #'   accordingly
 #' @return List of slendr populations
 #' @examples
-#' tree_populations(4, 1000, 50)
-#' tree_populations(6, 200, 500)
-random_populations <- function(n_populations, population_size, simulation_length) {
-  # todo: sanity check: if (npops < 1) stop(...), stop("blah blah", call. = FALSE)
+#' tree_populations(n_populations = 4, population_size = 1000,
+#'   simulation_length = 50)
+#' tree_populations(n_populations = 6, population_size = 200,
+#'   simulations_length = 500)
+random_populations <- function(n_populations, population_size,
+                               simulation_length) {
+
+  if (n_populations < 2) {
+    stop("At least 2 populations must be created", call. = FALSE)
+  }
+
   tree <- ape::rtree(n_populations)
   populations <- tree_populations(tree, population_size, simulation_length)
   return(populations)
 }
 
-#' Create a slendr model based on a given tree and a given number of gene flow
+#' Create model based on given tree
+#'
+#' Creates a slendr model based on a given tree and a given number of gene flow
 #' events. The function \code{tree_populations} is used to get a list of
 #' populations from given tree. Gene flow events between these populations are
 #' created randomly.
@@ -74,9 +98,11 @@ random_populations <- function(n_populations, population_size, simulation_length
 #' @return A slendr model
 #' @examples
 #' tree <- ape::rtree(4)
-#' tree_model(tree, 1000, 3, 0.5, 100)
+#' tree_model(tree = tree, population_size = 1000, n_gene_flow = 3,
+#'   rate_gene_flow = 0.5, simulation_length = 100)
 #' tree <- ape::rtree(6)
-#' tree_model(tree, 200, 4, c(0.2, 0.9), 1000)
+#' tree_model(tree = tree, population_size = 200, n_gene_flow = 4,
+#'   rate_gene_flow = c(0.2, 0.9), simulation_length = 1000)
 tree_model <- function(tree, population_size, n_gene_flow,
                        rate_gene_flow = c(0.01, 0.99), simulation_length) {
 
@@ -88,7 +114,9 @@ tree_model <- function(tree, population_size, n_gene_flow,
   return(model)
 }
 
-#' Create a slendr model based on a given number of populations and a given
+#' Create random model
+#'
+#' Creates a slendr model based on a given number of populations and a given
 #' number of gene flow events. A random ape tree is created according to the
 #' specified number of populations. With this tree, the function
 #' \code{tree_model} is called to create the model.
@@ -103,8 +131,10 @@ tree_model <- function(tree, population_size, n_gene_flow,
 #'   accordingly
 #' @return A slendr model
 #' @examples
-#' tree_model(4, 1000, 3, 0.5, 100)
-#' tree_model(6, 200, 4, c(0.2, 0.9), 1000)
+#' tree_model(n_populations = 4, population_size = 1000, n_gene_flow = 3,
+#'   rate_gene_flow = 0.5, simulation_length = 100)
+#' tree_model(n_populations = 6, population_size = 200, n_gene_flow = 4,
+#'   rate_gene_flow = c(0.2, 0.9), simulation_length = 1000)
 random_model <- function(n_populations, population_size, n_gene_flow,
                          rate_gene_flow = c(0.01, 0.99), simulation_length) {
 
@@ -123,10 +153,13 @@ recursion <- function(tree, current_node, parent_population, population_size,
   right <- children[2]
 
   if (!is.na(left)){
+    # adjust the edge length of node by the scale
     length <- ceiling(ape::node.depth.edgelength(tree)[left] * scale)
+    # check that parent and child can not have the same length
     if(length == attr(parent_population, "history")[[1]]$time) {
       length <- length + 1
     }
+
     env$populations[[left]] <- population(paste0("pop", left), time = length,
                                           N = population_size,
                                           parent = parent_population)
@@ -134,9 +167,6 @@ recursion <- function(tree, current_node, parent_population, population_size,
     recursion(tree, left, env$populations[[left]], population_size, env, scale)
   }
   if (!is.na(right)){
-    length <- floor(ape::node.depth.edgelength(tree)[right] * scale) + 1
-    #env$populations[[right]] <- population(paste0("pop", right), time = length,
-    #N = population_size, parent = env$populations[[current_node]])
     recursion(tree, right, parent_population, population_size, env, scale)
   }
 }
@@ -148,32 +178,31 @@ random_gene_flow <- function(populations, n, rate, simulation_length) {
 
       number1 <- sample(1:length(populations), 1)
       number2 <- sample(1:length(populations), 1)
+      # check that gene flow not between same population
       while(number1 == number2){
         number2 <- sample(1:length(populations), 1)
       }
       pop1 <- populations[[number1]]
       pop2 <- populations[[number2]]
+      # find out which population was created later to use as possible start for
+      # gene flow
       max_time <- max(attr(pop1, "history")[[1]]$time,
                     attr(pop2, "history")[[1]]$time)
       start_gf <- sample((max_time + 1):(simulation_length - 2), 1)
       end_gf <- sample((start_gf + 1):(simulation_length), 1)
+
+      # given rate of gene flow can be either list of min/max or single value
       if (length(rate) == 2){
+        if (rate[1] > rate[2]) {
+          stop("No valid range for gene flow rate", call. = FALSE)
+        }
         rate_gf <- runif(1, rate[1], rate[2])
       } else {
         rate_gf <- rate
       }
-      gf[[i]] <- gene_flow(from = pop1, to = pop2, start = start_gf, end = end_gf, rate_gf)
+      gf[[i]] <- gene_flow(from = pop1, to = pop2, start = start_gf,
+                           end = end_gf, rate_gf)
     }
   }
   return(gf)
 }
-
-# test calls
-#tree <- ape::rtree(4)
-#pops <- tree_populations(tree, 1000, 50)
-#pops2 <- random_populations(3, 1000, 100)
-#model <- tree_model(tree, 1000, 3, c(0.2, 0.9), 100)
-#model2 <- random_model(3, 1000, 2, 0.5, 1000)
-# plot_model(model, proportions = TRUE)
-# plot_model(model2, proportions = TRUE)
-
--- tests/testthat/test-model-generation.R
+++ tests/testthat/test-model-generation.R
@@ -1,56 +1,138 @@
-
-
 test_that("number of populations in tree_populations", {
   tree <- ape::rtree(4)
-  tree_pops <- tree_populations(tree, 1000, 50)
+  tree_pops <- tree_populations(tree = tree, population_size = 1000,
+                                simulation_length = 50)
   expect_equal(length(tree_pops), 4)
-  # 1, 0
+  tree <- ape::rtree(2)
+  tree_pops <- tree_populations(tree = tree, population_size = 750,
+                                simulation_length = 100)
+  expect_equal(length(tree_pops), 2)
+
+  tree <- ape::rtree(1)
+  expect_error(tree_populations(tree = tree, population_size = 50,
+                                simulation_length = 1000),
+               "At least 2 populations must be created")
 })
 
 test_that("number of populations in random_populations", {
-  random_pops <- random_populations(5, 1000, 50)
+  random_pops <- random_populations(n_populations = 5, population_size = 100,
+                                    simulation_length = 500)
   expect_equal(length(random_pops), 5)
+  random_pops <- random_populations(n_populations = 2, population_size = 10,
+                                    simulation_length = 7500)
+  expect_equal(length(random_pops), 2)
+
+  expect_error(random_populations(n_populations = 0, population_size = 800,
+                                  simulation_length = 1500),
+               "At least 2 populations must be created")
+  expect_error(random_populations(n_populations = 1, population_size = 70,
+                                  simulation_length = 250),
+               "At least 2 populations must be created")
 })
 
 test_that("number of populations in tree_model", {
   tree <- ape::rtree(3)
-  tree_model <- tree_model(tree, 1000, 3, c(0.2, 0.9), 100)
+  tree_model <- tree_model(tree = tree, population_size = 900, n_gene_flow = 3,
+                           rate_gene_flow = c(0.2, 0.8),
+                           simulation_length = 600)
   expect_equal(length(tree_model$populations), 3)
   expect_equal(nrow(tree_model$splits), 3)
 })
 
 test_that("number of populations in random_model", {
-  random_model <- random_model(6, 1000, 3, c(0.2, 0.9), 100)
+  random_model <- random_model(n_populations = 6, population_size = 580,
+                               n_gene_flow = 4,
+                               rate_gene_flow = 0.6,
+                               simulation_length = 400)
   expect_equal(length(random_model$populations), 6)
   expect_equal(nrow(random_model$splits), 6)
 })
 
 test_that("number of gene flow events in tree_model", {
   tree <- ape::rtree(3)
-  tree_model <- tree_model(tree, 1000, 3, c(0.2, 0.9), 100)
+  tree_model <- tree_model(tree = tree, population_size = 8000, n_gene_flow = 3,
+                           rate_gene_flow = c(0.3, 0.4),
+                           simulation_length = 1000)
   expect_equal(nrow(tree_model$geneflow), 3)
-  tree_model <- tree_model(tree, 1000, 0, c(0.2, 0.9), 100)
+  tree_model <- tree_model(tree = tree, population_size = 40, n_gene_flow = 0,
+                           rate_gene_flow = 0.1,
+                           simulation_length = 540)
   expect_equal(nrow(tree_model$geneflow), NULL)
-  ######### is this correct???
 })
 
 test_that("number of gene flow events in random_model", {
-  random_model <- random_model(6, 1000, 4, c(0.2, 0.9), 100)
+  random_model <- random_model(n_populations = 6, population_size = 400,
+                               n_gene_flow = 4,
+                               rate_gene_flow = c(0.6, 0.8),
+                               simulation_length = 600)
   expect_equal(nrow(random_model$geneflow), 4)
+
+  random_model <- random_model(n_populations = 100, population_size = 3000,
+                               n_gene_flow = 50,
+                               rate_gene_flow = 0.9,
+                               simulation_length = 2300)
+  expect_equal(nrow(random_model$geneflow), 50)
 })
 
+test_that("rate gene flow in given range", {
+  tree <- ape::rtree(6)
+  tree_model <- tree_model(tree = tree, population_size = 60, n_gene_flow = 12,
+                           rate_gene_flow = c(0.2, 0.9),
+                           simulation_length = 1800)
+  expect_true(max(tree_model$geneflow$rate) <= 0.9)
+  expect_true(min(tree_model$geneflow$rate) >= 0.2)
 
-# todo test range and fixed value
+
+  tree <- ape::rtree(7)
+  tree_model <- tree_model(tree = tree, population_size = 520, n_gene_flow = 8,
+                           rate_gene_flow = c(0.4, 0.7),
+                           simulation_length = 6100)
+  expect_true(max(tree_model$geneflow$rate) <= 0.7)
+  expect_true(min(tree_model$geneflow$rate) >= 0.4)
+
+  tree <- ape::rtree(9)
+  expect_error(tree_model(tree = tree, population_size = 340, n_gene_flow = 6,
+                          rate_gene_flow = c(0.7, 0.4),
+                          simulation_length = 560),
+               "No valid range for gene flow rate")
+})
+
+test_that("rate gene flow equal to given fixed value", {
+  tree <- ape::rtree(5)
+  tree_model <- tree_model(tree = tree, population_size = 700, n_gene_flow = 5,
+                           rate_gene_flow = 0.5,
+                           simulation_length = 420)
+  expect_true(max(tree_model$geneflow$rate) == 0.5)
+  expect_true(min(tree_model$geneflow$rate) == 0.5)
+
+  tree <- ape::rtree(8)
+  tree_model <- tree_model(tree = tree, population_size = 1000, n_gene_flow = 14,
+                           rate_gene_flow = 0.2,
+                           simulation_length = 4800)
+  expect_true(max(tree_model$geneflow$rate) == 0.2)
+  expect_true(min(tree_model$geneflow$rate) == 0.2)
+})
 
 test_that("simulation length in tree_model", {
   tree <- ape::rtree(3)
-  tree_model <- tree_model(tree, 1000, 3, c(0.2, 0.9), 100)
+  tree_model <- tree_model(tree = tree, population_size = 290, n_gene_flow = 3,
+                           rate_gene_flow = c(0.4, 0.7),
+                           simulation_length = 100)
   expect_equal(tree_model$orig_length, 100)
 })
 
 test_that("simulation length in random_model", {
-  random_model <- random_model(6, 1000, 4, c(0.2, 0.9), 1000)
+  random_model <- random_model(n_populations = 6, population_size = 25,
+                               n_gene_flow = 4, rate_gene_flow = c(0.1, 0.8),
+                               simulation_length = 1000)
   expect_equal(random_model$orig_length, 1000)
+})
 
-# todo test to make sure that populations not created after simulation ends
+test_that("all populations created before simulation ends", {
+  tree <- ape::rtree(8)
+  tree_model <- tree_model(tree = tree, population_size = 800, n_gene_flow = 8,
+                           rate_gene_flow = 0.5,
+                           simulation_length = 450)
+  expect_true(max(sapply(tree_model$populations,
+                         function(p) attr(p, "history")[[1]]$time)) < 450)
 })
